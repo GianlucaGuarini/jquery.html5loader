3527ef93c255ef75b28fe5aa88da9eb1b26bb2ba
toxinhead <toxinhead@gmail.com>
2014-01-30 04:15:01 +0100

    Script errors log with debugMode

--- src/jquery.html5Loader.js
+++ src/jquery.html5Loader.js
@@ -348,7 +348,7 @@
     var loadScript = function(file) {
       var defer = new $.Deferred(),
         size = file.size;
-      $.getScript(file.source, function(data) {
+      $.getScript(file.source).done(function(data) {
 
         log('File Loaded:' + file.source);
 
@@ -360,6 +360,9 @@
         _files.splice(0, 1);
         updatePercentage();
         defer.resolve();
+      })
+      .fail(function(jqxhr, settings, exception) {
+        log('File Failed:' + file.source, exception);
       });
 
       return defer.promise();
