923c7364e0d45a65605b8f34af851661b77532e5
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-02-01 18:30:53 +0100

    new v1.6.8

--- files/test.js
+++ files/test.js
@@ -1,1 +1,3 @@
  //it could be a javascript module for example google analytics
+if (window.console)
+	window.console.log('script loaded');
---
+++ files/test2.js
@@ -0,0 +1,2 @@
+if (window.console)
+	window.console.log('this script should not be executed by plugin options');
